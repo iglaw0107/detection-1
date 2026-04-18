39f089300e50798aac8c2ec72c09f2df6a9e8ca8
Shivam000189 <shivamsharmass9897@gmail.com>
2026-04-18 18:32:14 +0530

    model download setup

--- ai-model/start.sh
+++ ai-model/start.sh
@@ -2,14 +2,12 @@
 
 echo "🚀 Starting CrimeAI ML service..."
 
-# create models folder
 mkdir -p models
 
-# train model if not exists
+# Download model if not exists
 if [ ! -f "models/crime_model.pkl" ]; then
-  echo "🤖 Training model..."
-  python train.py
+  echo "⬇️ Downloading model..."
+  curl -L "https://drive.google.com/uc?export=download&id=1kOSfD6BzPxCmz1EHvmil0S6m0HXUcnrg" -o models/crime_model.pkl
 fi
 
-# start server
 gunicorn app:app --bind 0.0.0.0:$PORT
